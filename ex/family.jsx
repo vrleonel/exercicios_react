00ef5bac0ee6700a6709f88e43bfd67bd1bf6494
Vitor Leonel <vitor.leonel@loggi.com>
2017-08-25 08:18:42 -0300

    Component with children

--- ex/family.jsx
+++ ex/family.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
+import {childrenWithProps} from '../utils/reactUtils.js';
 
 export default props => (
   <div>
     <h1> Família </h1>
-    { React.Children.map( props.children, 
-                          child => React.cloneElement(child, {...props}) ) }
+    {childrenWithProps(props.children, props)}
   </div>
-)
+);
